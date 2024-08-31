d75405dec2902e0a015bdd96af5d02c06363dc3a
EliaOndacs <amirreza.ondacs90@gmail.com>
2024-08-31 14:48:44 +0330

    "Updated setup.py: added 'ansi' to install requirements, added license and classifiers"

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ setup(
     name="tent",
     version="0.1.0",
     packages=find_packages(),
-    install_require=[],
+    install_require=["ansi"],
     author="EliaOndacs",
     author_email="amirreza.ondacs90@gmail.com",
     description="a really really basic and low level app framework",
     url="https://github.com/EliaOndacs/TENT",
+    license="MIT LICENSE"
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
